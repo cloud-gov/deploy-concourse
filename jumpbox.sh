227498a82aaaf2822f59b9ae3b3cba98e69074bd
M. Adam Kendall <michael.kendall@gsa.gov>
2016-06-22 10:56:13 -0400

    Initial take on temporary jumpbox

---
+++ jumpbox.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# vim: set ft=sh
+
+set -e
+
+#
+# A little environment validation
+#
+if [ -z "$BOSH_TARGET" ]; then
+  echo "must specify \$BOSH_TARGET" >&2
+  exit 1
+fi
+if [ -z "$BOSH_USERNAME" ]; then
+  echo "must specify \$BOSH_USERNAME" >&2
+  exit 1
+fi
+if [ -z "$BOSH_PASSWORD" ]; then
+  echo "must specify \$BOSH_PASSWORD" >&2
+  exit 1
+fi
+if [ -z "$BOSH_CACERT" ]; then
+  echo "must specify \$BOSH_CACERT" >&2
+  exit 1
+fi
+#
+# Run the errand for the appropriate deployment
+#
+bosh --ca-cert $BOSH_CACERT -n target $BOSH_TARGET
+bosh login <<EOF 1>/dev/null
+$BOSH_USERNAME
+$BOSH_PASSWORD
+EOF
