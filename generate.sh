91ceec35367d81c4a9876e2b31d8a606af22cc29
M. Adam Kendall <michael.kendall@gsa.gov>
2016-09-08 15:23:53 -0400

    Converts the deployment to use spruce instead of spiff

--- generate.sh
+++ generate.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-spiff merge \
-  concourse.yml \
-  secrets.yml \
-  > manifest.yml
+set -e -x
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+SECRETS=$SCRIPTPATH/secrets.yml
+MANIFEST=$SCRIPTPATH/manifest.yml
+if [ ! -z "$1" ]; then
+  SECRETS=$1
+fi
+if [ ! -z "$2" ]; then
+  MANIFEST=$2
+fi
+
+spruce merge \
+  $SCRIPTPATH/concourse.yml \
+  $SECRETS \
+  > $MANIFEST
