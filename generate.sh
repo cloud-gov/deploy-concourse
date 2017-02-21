1c19441f551f993fcb353adffadd7a60f2368b44
Joshua Carp <jm.carp@gmail.com>
2017-02-21 14:47:09 -0500

    Use secrets from terraform outputs.

--- generate.sh
+++ generate.sh
@@ -4,16 +4,26 @@ set -e -x
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
+CONFIG=""
 SECRETS=$SCRIPTPATH/secrets.yml
+TERRAFORM=$SCRIPTPATH/terraform.yml
 MANIFEST=$SCRIPTPATH/manifest.yml
 if [ ! -z "$1" ]; then
-  SECRETS=$1
+  CONFIG=$1
 fi
 if [ ! -z "$2" ]; then
-  MANIFEST=$2
+  SECRETS=$2
+fi
+if [ ! -z "$3" ]; then
+  TERRAFORM=$3
+fi
+if [ ! -z "$4" ]; then
+  MANIFEST=$4
 fi
 
-spruce merge \
+spruce merge --prune meta --prune terraform_outputs \
   $SCRIPTPATH/concourse.yml \
+  $CONFIG \
   $SECRETS \
+  $TERRAFORM \
   > $MANIFEST
