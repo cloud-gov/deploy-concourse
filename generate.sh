3d3534125ec430533ba52a1ffc1b2b6c81d84406
Chris Nelson <christopher.nelson@gsa.gov>
2017-05-16 08:34:49 -0700

    move tenant environment config out of secrets

--- generate.sh
+++ generate.sh
@@ -4,27 +4,8 @@ set -e -x
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
-CONFIG=""
-SECRETS=$SCRIPTPATH/secrets.yml
-TERRAFORM=$SCRIPTPATH/terraform.yml
-MANIFEST=$SCRIPTPATH/manifest.yml
-if [ ! -z "$1" ]; then
-  CONFIG=$1
-fi
-if [ ! -z "$2" ]; then
-  SECRETS=$2
-fi
-if [ ! -z "$3" ]; then
-  TERRAFORM=$3
-fi
-if [ ! -z "$4" ]; then
-  MANIFEST=$4
-fi
-
 spruce merge --prune meta --prune terraform_outputs \
   $SCRIPTPATH/concourse.yml \
   $SCRIPTPATH/rds-ca-cert.yml \
-  $CONFIG \
-  $SECRETS \
-  $TERRAFORM \
-  > $MANIFEST
+  $@ \
+> concourse-manifest/manifest.yml
