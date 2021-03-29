659119d4e72c09cfe8577fe92c8c76b477a99831
Ben Berry <ben.berry@gsa.gov>
2021-03-29 11:09:42 -0700

    convert relative paths to absolute paths

--- jumpbox.sh
+++ jumpbox.sh
@@ -7,5 +7,8 @@ cat <<EOF >> $HOME/.bashrc
 PS1="\[\$(tput setaf "$PROMPT_COLOR")\]\[\e]0;\u@$BOSH_DIRECTOR_NAME: \w\a\]${debian_chroot:+($debian_chroot)}\u@$BOSH_DIRECTOR_NAME:\w\$ \[\$(tput sgr0)\]"
 EOF
 
+export CREDHUB_CA_CERT="$(pwd)/${CREDHUB_CA_CERT}"
+export BOSH_CACERT="$(pwd)/${BOSH_CACERT}"
+
 # Hack: Fail build to allow intercept; see https://github.com/concourse/concourse/issues/1160
 exit 1
