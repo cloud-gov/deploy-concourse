05005551c34ac51abc80bb6b697ec16705aad381
Dorothy Tamasang <dorothy.tamasang@gsa.gov>
2022-10-31 13:27:47 -0400

    modified jumpbox.test.sh

--- jumpbox.test.sh
+++ jumpbox.test.sh
@@ -5,7 +5,6 @@ set -e -u
 
 # add some stuff to bashrc. Note that the variables and commands are evaluated when jumpbox.sh is executed
 # NOT when .bashrc is, so we don't have to worry about rerunning things
-cat <<EOF >> $HOME/.bashrc
 # add director name to terminal to reduce chances of running commands in the wrong env
 
 
